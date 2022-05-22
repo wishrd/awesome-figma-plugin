981731062f7029c68320fb0e3a5684f81e7ee240
wishrd <wish.rd@gmail.com>
2022-05-22 17:37:03 +0200

    feat(Detail): print title and image

--- src/ui/app/Detail.tsx
+++ src/ui/app/Detail.tsx
@@ -1,11 +1,34 @@
 import '../styles/styles.scss';
 
 import * as React from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 const Detail = ({}) => {
-    return (
-        <div>Detail! as asdasd</div>
-    );
+  const { eventId } = useParams();
+  const [event, setEvent] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('https://my-json-server.typicode.com/wishrd/awesome-figma-plugin-db/events/' + eventId, {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+      }
+    })
+      .then(res => res.json())
+      .then(result => setEvent(result), err => console.error('Ups! error fetching the event', err))
+  }, []);
+
+  return (
+    <div>
+      <h1><Link to="/">Back</Link> {event?.name}</h1>
+      <dl>
+        <dt>Name</dt>
+        <dd>{event?.name}</dd>
+        <dt>Image</dt>
+        <dd><img src={event?.image} width="100" height="100" /></dd>
+      </dl>
+    </div>
+  );
 };
 
 export default Detail;
