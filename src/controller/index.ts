cf4b4b5e236eabca2fa040c422d4bdf0a48dd535
Pablo Díez <wish.rd@gmail.com>
2022-05-26 17:51:30 +0200

    fix: remove node check

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -4,11 +4,6 @@ import { setNodeImage } from './utils/set-node-image';
 figma.showUI(__html__, { width: 360, height: 400 });
 
 figma.ui.onmessage = msg => {
-  const node = figma.currentPage.selection[0];
-  if (!node?.id) {
-    return;
-  }
-
   if (msg.type === 'set-text') {
     setNodeText(msg.data);
   } else if (msg.type === 'set-image') {
--- src/ui/app/Detail.tsx
+++ src/ui/app/Detail.tsx
@@ -39,7 +39,6 @@ const Detail = ({}) => {
         }
       }, '*');
     });
-
   }
 
   return (
